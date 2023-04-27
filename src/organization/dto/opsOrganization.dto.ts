99cddde3fb20b229838fefa2d81b7c8603e58336
csanlucas <csanlucas@live.com>
2023-04-26 20:32:23 -0500

    Add validation on parameters (#4)

--- src/organization/dto/opsOrganization.dto.ts
+++ src/organization/dto/opsOrganization.dto.ts
@@ -1,9 +1,22 @@
+import { IsString, IsNumber, IsNotEmpty, MaxLength } from 'class-validator'
+import { NAME_ATTR_MAX_LENGTH } from '../entities/constants';
+
 export class CreateOrganizationDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(NAME_ATTR_MAX_LENGTH)
     name: string;
+
+    @IsNumber()
     status: number;
 }
 
 export class UpdateOrganizationDto {
+    @IsString()
+    @IsNotEmpty()
+    @MaxLength(NAME_ATTR_MAX_LENGTH)
     name: string;
+
+    @IsNumber()
     status: number;
 }
